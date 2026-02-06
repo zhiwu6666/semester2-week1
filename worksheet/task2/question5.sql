4a2f13d1375655d8efb535d863517a96f87d248d
JiaHe ZHU <hvgx0657@leeds.ac.uk>
2026-02-06 13:55:12 +0000

    Adjust task2 question5 output

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -6,9 +6,9 @@ SELECT
 	s.StudentId,
 	s.FirstName,
 	s.LastName,
-	COALESCE(SUM(c.Credits), 0) AS TotalCreditsPassed
+	SUM(c.Credits) AS TotalCreditsPassed
 FROM Student AS s
-LEFT JOIN (
+JOIN (
 	SELECT
 		e.StudentId,
 		e.CourseId
@@ -19,7 +19,7 @@ LEFT JOIN (
 	HAVING MAX(e.Grade) >= 40
 ) AS passed
 	ON passed.StudentId = s.StudentId
-LEFT JOIN Course AS c
+JOIN Course AS c
 	ON c.CourseId = passed.CourseId
 GROUP BY
 	s.StudentId,
